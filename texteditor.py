c26b24f6964101d804ba10f68e04d1e3e9c47396
SG4 <59972034+SoGreeno@users.noreply.github.com>
2021-03-07 22:34:57 +0100

    Notatnik ale w pythonie

---
+++ texteditor.py
@@ -0,0 +1,8 @@
+# chyba nie w pełni funkcjonalne
+import time
+editingnow = input("Wybierz nazwę pliku w folderze:")
+plik = open(editingnow, "w")
+print("TextEditor v1.0")
+plik.write( input("& "))
+print("Zapisywanie...")
+time.sleep(1)
